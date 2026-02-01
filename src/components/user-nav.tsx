c5364f89f918152c6d299f77169083aff580647a
Viraj Makwana <87859781+Regentos4910@users.noreply.github.com>
2026-02-02 00:28:33 +0530

    Update user avatar and name in user navigation

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -18,16 +18,16 @@ export function UserNav() {
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-9 w-9">
             
-            <AvatarFallback>JD</AvatarFallback>
+            <AvatarFallback>VM</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">Jane Doe</p>
+            <p className="text-sm font-medium leading-none">Viraaj Makwana</p>
             <p className="text-xs leading-none text-muted-foreground">
-              jane.doe@example.com
+              regentos4910@gmail.com
             </p>
           </div>
         </DropdownMenuLabel>
